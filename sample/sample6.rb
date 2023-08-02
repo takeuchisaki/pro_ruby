d66113eab2c7b01fa4612dd2e048c7df639d9e4a
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-08-02 08:56:26 +0000

    Finish6.2

---
+++ sample/sample6.rb
@@ -0,0 +1,62 @@
+text = <<TEXT
+I love Ruby.
+Python is a great language.
+Java and JavaScript are different.
+TEXT
+
+text.scan(/[A-Z][A-Z-a-z]+/)
+
+
+
+text = <<TEXT
+私の郵便番号は1234567です。
+僕の住所は6770056兵庫県西脇市板波町1234だよ。
+TEXT
+
+puts text.gsub(/(\d{3})(\d{4})/) { "#{$1}-#{$2}"}
+
+
+
+text = <<TEXT
+名前：伊藤淳一
+電話：03(1234)5678
+電話：9999-99-9999
+電話：03(1234-5678
+電話：090-1234-5678
+電話：0795(12)3456
+電話：04992-1-2345
+住所：兵庫県西脇市板波町1-2-3
+TEXT
+
+numbers = text.scan (/0[0-9]\d{0,3}[-(]\d{1,4}[-)]\d{4}/)
+
+numbers.grep(/\(\d+\)|-\d+-/)
+
+
+
+text = <<-TEXT
+クープバゲットのパンは美味しかった。
+今日はクープ バゲットさんに行きました。
+クープ　バゲットのパンは最高。
+ジャムおじさんのパン、ジャムが入ってた。
+また行きたいです。クープ・バゲット。
+クープ・バケットのパン、売り切れだった（><）
+TEXT
+
+text.split(/\n/).grep(/クープ.?バ[ゲケ]ット/)
+
+
+
+html = <<-HTML
+<select name="game_console">
+<option value="none"></option>
+<option value="wii_u" selected>Wii U</option>
+<option value="ps4">プレステ4</option>
+<option value="gb">ゲームボーイ</option>
+</select>
+HTML
+
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+
+puts replaced
+
