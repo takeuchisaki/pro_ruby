f0047faa881b4bff340f1957adc6352fdcd04b32
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-08-13 04:22:56 +0000

    Finish7章

--- sample/sample7.rb
+++ sample/sample7.rb
@@ -1,20 +1,51 @@
 class Product
+  # Product.freeze
   attr_render :name, :price
   
-  def initialize(name, price)
-    @name = name
-    @price = price
+  def to_s
+    "name: #{name}"
   end
+  
+  private
+  
+  def name
+    'A great movie'
+  end
+  
+  # def initialize(name, price)
+  #   @name = name
+  #   @price = price
+  # end
+  
 end
 
 class DVD < Priduct
   attr_render :running_time
   
-  def initialize(name,price, running_time)
-    super(name, price)
-    @running_time = running_time
+  # def initialize(name,price, running_time)
+  #   super(name, price)
+  #   @running_time = running_time
+  # end
+  
+  private
+  
+  def name
+    'A awesome film'
   end
+  
+  # def to_s
+  #   "name: #{name}"
+  # end
   
 end
 
 product = Product.new('A great movie', 1000, 120)
+
+
+class CountryCode
+  attr_reader :code
+  
+  def initialize(code)
+    @code = code
+  end
+end
