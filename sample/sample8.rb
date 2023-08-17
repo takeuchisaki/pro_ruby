68dde236573971e228e9c30b8ff7d9e16418e232
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-08-17 03:59:32 +0000

    Finish8章

--- sample/sample8.rb
+++ sample/sample8.rb
@@ -1,8 +1,8 @@
-# puts "Hello, world!".rainbow
+puts "Hello, world!".rainbow
 
-# puts [1, 2, 3].rainnbow
+puts [1, 2, 3].rainnbow
 
-# puts "\e[31mABC\e[0m"
+puts "\e[31mABC\e[0m"
 
 
 module Greetable
@@ -40,4 +40,54 @@ end
 class User
   p self
   p self.class
+end
+
+
+
+module Baseball
+  class Secound
+    def intialize(player, uniform_number)
+      @player = player
+      @uniform_number = uniform_number
+    end
+  end
+end
+
+Basaball::Secound.new("Alice, 13")
+
+
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+  
+  module_function :log
+end
+
+
+class Product
+  include Loggable
+  
+  def title
+    log "title is called."
+    "A great movie"
+  end
+  
+end
+
+
+
+module NameDecorrator
+  def name
+    "<<#{super}>>"
+  end
+end
+
+class Product
+  prepend NameDecorrator
+  
+  # def name
+  #   "A great film"
+  # end
+  
 end
