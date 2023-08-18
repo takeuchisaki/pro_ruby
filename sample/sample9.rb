6c1a8370faf989cd943a834371aee7e8e8e70bff
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-08-18 03:16:03 +0000

    Finish9.2

---
+++ sample/sample9.rb
@@ -0,0 +1,26 @@
+puts "Start"
+
+begin
+  1 + "10"
+rescue
+  puts "例外が発生したが、このまま実行する"
+end
+
+puts "End."
+
+
+
+retry_count = 0
+
+begin
+  puts "処理を開始します。"
+  1 / 0
+rescue
+  retry_count += 1
+  if retry_count <= 3
+    puts "retryします。（#{retry_count}回目"
+    retry
+  else
+    puts "retryに失敗しました。"
+  end
+end
